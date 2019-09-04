837edf42639fc49948b6c9c63fadf168a5ce9f23
masonparsons <masonparsons711@yahoo.com>
2019-09-04 09:14:23 -0700

    Update main.c

--- jumpers/main.c
+++ jumpers/main.c
@@ -5,11 +5,35 @@
 //  Created by Parsons, Mason on 9/4/19.
 //  Copyright © 2019 Parsons, Mason. All rights reserved.
 //
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+main(void) {
+    
+    float amount;
+    char name[15];
+    printf("name your jumpers\n");
+    scanf("%s",&name[15]);
+    printf("how many jumps\n");
+    scanf("%f",&amount);
+    
+    
+    int countdown = amount;                  char* bedjumpers = &name[15];
+    
+    
+    int i = countdown;
+    while (i > -1) {
+        
+        if (i == 0) {
+            printf("Zero little %s", bedjumpers);
+        } else {
+            
+            printf("%i little %s jumping on the bed\n", i, bedjumpers);
+            printf("One fell off and broke his head\n");
+            printf("Mama called the doctor and the doctor said\n");
+            printf("no more %s jumping on the bed.\n",bedjumpers);
+        }
+        
+        
+        i--;
+    }
 }
